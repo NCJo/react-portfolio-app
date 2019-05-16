36ad9163b011829096edce5f42c5090fad53ce7c
NCJo <cnimkulr@gmail.com>
2019-05-16 00:17:25 -0600

    Initialize Repository

--- src/App.js
+++ src/App.js
@@ -1,26 +1,52 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from 'react';
+import Projects from './Projects';
+import SocialProfiles from './SocialProfiles'
+import profile from './assets/profile.png';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+export default class App extends Component {
+    state = { displayBio : false };
+
+    // constructor() {
+    //     super();
+    //     this.state = { displayBio: false };
+    //     console.log('constructor this', this);
+
+    //     this.toggleDisplayBio = this.toggleDisplayBio.bind(this);
+       
+    // }
+
+    toggleDisplayBio = () => {
+        this.setState({ displayBio: !this.state.displayBio });
+    }
+
+    render() { 
+        return (
+            <div>
+                <img src={profile} className='profile' />
+                <h1>Yo</h1>
+                <p>The name is X. I'm a software engineer.</p>
+                <p>I'm always looking forward to tackle any challenge.</p>
+                {
+                    this.state.displayBio ? (
+                        <div>
+                            <p>I live in NC.</p>
+                            <p>My favorite language is Python, but ReactJS is also badass.</p>
+                            <p>A skateboard Enthusiast.</p>
+                            <button onClick={this.toggleDisplayBio}>Show Less</button>
+                        </div>
+                    ) : (
+                        <div>
+                            <button onClick={this.toggleDisplayBio}>Read More</button>
+                        </div>
+                    )
+                }
+                <hr />
+                <Projects />
+                <hr />
+                <SocialProfiles />
+                
+            </div>
+        )
+    }
 }
-
-export default App;
---
+++ src/SocialProfiles.js
@@ -0,0 +1,34 @@
+import React, { Component } from 'react';
+import SOCIAL_PROFILES from './data/socialProfiles.js';
+
+class SocialProfile extends Component {
+    render() {
+        const { link, image } = this.props.socialProfile;
+        return (
+            <span>
+                <a href={link}>
+                <img src={image} style={{ width: 35, height: 35, margin: 10}} /></a>
+            </span>
+        )
+    }
+}
+
+
+export default class SocialProfiles extends Component {
+    render() {
+        return (
+            <div>
+                <h2>Connect with me!</h2>
+                <div>
+                    {
+                        SOCIAL_PROFILES.map(SOCIAL_PROFILE => {
+                            return (
+                                <SocialProfile key={SOCIAL_PROFILE.id} socialProfile={SOCIAL_PROFILE}/>
+                            );
+                        })
+                    }
+                </div>
+            </div>
+        )
+    }
+}
