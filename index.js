7fd3d6e889ad7665b52f7880b0a21de66869aab4
NCJo <cnimkulr@gmail.com>
2019-06-10 09:07:22 -0600

    fix location of listed index.html on express file 2

--- index.js
+++ index.js
@@ -7,8 +7,8 @@ app.use(express.static(path.join(__dirname, 'dist')));
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => console.log(`listening on the ${PORT}`));
 
-app.get('/interview*', function(req, res) {
-    res.sendFile(path.join(__dirname, './src/index.html'), function(err) {
+app.get('*', function(req, res) {
+    res.sendFile(path.join(__dirname, '/src/index.html'), function(err) {
       if (err) {
         res.status(500).send(err)
       }
