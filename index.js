72a656ebb5809ced001c2bfb43f3ef359df8eee8
NCJo <cnimkulr@gmail.com>
2019-06-10 09:00:59 -0600

    add catch-all function in express

--- index.js
+++ index.js
@@ -6,3 +6,11 @@ app.use(express.static(path.join(__dirname, 'dist')));
 
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => console.log(`listening on the ${PORT}`));
+
+app.get('/interview*', function(req, res) {
+    res.sendFile(path.join(__dirname, '/index.html'), function(err) {
+      if (err) {
+        res.status(500).send(err)
+      }
+    })
+  })
