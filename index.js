72c8ef268df03eca01146587c869c4d921b255ec
NCJo <cnimkulr@gmail.com>
2019-06-10 09:14:53 -0600

    Fixed: /GET error is completely resolved

--- index.js
+++ index.js
@@ -7,6 +7,8 @@ app.use(express.static(path.join(__dirname, 'dist')));
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => console.log(`listening on the ${PORT}`));
 
+
+// This can solve /GET error, it's a problem with react router
 app.get('*', function(req, res) {
     res.sendFile(path.join(__dirname, 'dist', 'index.html'), function(err) {
       if (err) {
