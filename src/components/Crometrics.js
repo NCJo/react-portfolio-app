2eae03d2b24730d92cc1a5c422a33c0993fc20ae
NCJo <cnimkulr@gmail.com>
2019-07-02 23:06:46 -0600

    fixed wrong image

--- src/components/Crometrics.js
+++ src/components/Crometrics.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import natural from '../assets/natural.png';
+import natural2 from '../assets/natural2.png';
 
 
 export default class Crometrics extends Component {
@@ -8,11 +8,11 @@ export default class Crometrics extends Component {
         return (
             <div>
                 <h2>Sample of the QA Report</h2>
-                <h3>Hello CroMetrics! Thank you for reading my job submission. Here is a doughnut. 🍩</h3>
+                <h3>Hello CroMetrics! Thank you for checking out my job submission. Here is a doughnut. 🍩</h3>
                 <h5>Company Link: <a href="https://www.nathab.com/">https://www.nathab.com/</a></h5>
                 <br />
                 <hr />
-                <img src={natural} alt='sample' style={{ width: 691, height: 797 }}/>
+                <img src={natural2} alt='sample' style={{ width: 691, height: 797 }}/>
             </div>
         )
     }
